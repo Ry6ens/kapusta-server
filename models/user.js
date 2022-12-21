30a14660962b24c4a106c5f5ae1f411bf3b38036
Sergey Gulaga <ry6ens@gmail.com>
2022-12-21 17:24:15 +0200

    fix accessToken

--- models/user.js
+++ models/user.js
@@ -1,65 +1,63 @@
 const Joi = require("joi");
-const {Schema, model} = require('mongoose');
+const { Schema, model } = require("mongoose");
 
-const {handleSaveErrors} = require("../helpers");
+const { handleSaveErrors } = require("../helpers");
 
-const emailRegexp = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+const emailRegexp =
+  /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     name: {
-        type: String,
-        required: [true, 'Set user name for user'],
-        minlength: 2,
+      type: String,
+      required: [true, "Set user name for user"],
+      minlength: 2,
     },
     password: {
-        type: String,
-        required: [true, 'Set password for user'],
-        minlength: 6,
+      type: String,
+      required: [true, "Set password for user"],
+      minlength: 6,
     },
     email: {
-        type: String,
-        required: [true, 'Email is required'],
-        unique: true,
-        match: emailRegexp,
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      match: emailRegexp,
     },
-    token: {
-        type: String,
-        default: ""
+    accessToken: {
+      type: String,
+      default: "",
     },
-}, {versionKey: false, timestamps: true});
+  },
+  { versionKey: false, timestamps: true }
+);
 
 userSchema.post("save", handleSaveErrors);
 
 const registerSchema = Joi.object({
-    email: Joi.string()
-            .pattern(emailRegexp)
-            .required(),
-    password: Joi.string()
-            .min(6)
-            .required(),
-    name: Joi.string()
-            .min(2)
-            .required(),
-})
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+  name: Joi.string().min(2).required(),
+});
 
 const loginSchema = Joi.object({
-    email: Joi.string().pattern(emailRegexp).required(),
-    password: Joi.string().min(6).required(),
-})
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+});
 
 const emailSchema = Joi.object({
-    email: Joi.string().pattern(emailRegexp).required(),
-})
+  email: Joi.string().pattern(emailRegexp).required(),
+});
 
-const User = model('user', userSchema);
+const User = model("user", userSchema);
 
 const schemas = {
-    registerSchema,
-    loginSchema,
-    emailSchema,
-}
+  registerSchema,
+  loginSchema,
+  emailSchema,
+};
 
 module.exports = {
-    User,
-    schemas,
+  User,
+  schemas,
 };
