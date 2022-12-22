1add07e99c0a27154638c5d032e651193b73a08e
Sergey Gulaga <ry6ens@gmail.com>
2022-12-22 10:09:15 +0200

    add console

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,74 +1,80 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const {Balance} = require("../models/balance");
+const { Balance } = require("../models/balance");
 
-const {User} = require("../models/user");
-const {SECRET_KEY} = process.env;
+const { User } = require("../models/user");
+const { SECRET_KEY } = process.env;
 
-const {RequestError} = require("../helpers");
+const { RequestError } = require("../helpers");
 
-const register = async(req, res)=> {
-    const {username, email, password, name} = req.body;
+const register = async (req, res) => {
+  const { username, email, password, name } = req.body;
 
-    const user = await User.findOne({email});
-    if(user) {
-        throw RequestError(409, "Email in use")
-    }
+  const user = await User.findOne({ email });
+  if (user) {
+    throw RequestError(409, "Email in use");
+  }
 
-    const hashPassword = await bcrypt.hash(password, 10)
+  const hashPassword = await bcrypt.hash(password, 10);
 
-    const newUser = await User.create({username, email, password: hashPassword, name});
+  const newUser = await User.create({
+    username,
+    email,
+    password: hashPassword,
+    name,
+  });
 
-    const owner = newUser._id;
+  const owner = newUser._id;
 
-    await Balance.create({owner})
+  await Balance.create({ owner });
 
-    res.status(201).json({
-        "user": {
-            "name": newUser.name,
-            "email": newUser.email,
-        }
-    })
+  res.status(201).json({
+    user: {
+      name: newUser.name,
+      email: newUser.email,
+    },
+  });
 };
 
-const login = async(req, res) => {
-    const {email, password} = req.body;
-    const user = await User.findOne({email});
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
 
-    if(!user) {
-        throw RequestError(401, "Invalid email or password");
-    }
+  if (!user) {
+    throw RequestError(401, "Invalid email or password");
+  }
 
-    const passwordCompare = await bcrypt.compare(password, user.password);
-    if(!passwordCompare) {
-        throw RequestError(401, "Invalid email or password");
-    }
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare) {
+    throw RequestError(401, "Invalid email or password");
+  }
 
-    const paylaod = {
-        id: user._id,
-    }
+  const paylaod = {
+    id: user._id,
+  };
 
-    const accessToken = jwt.sign(paylaod, SECRET_KEY, {expiresIn: "23h"})
-    await User.findByIdAndUpdate(user._id, {accessToken})
+  const accessToken = jwt.sign(paylaod, SECRET_KEY, { expiresIn: "23h" });
+  await User.findByIdAndUpdate(user._id, { accessToken });
 
-    res.json({
-        accessToken,
-        user: {
-            "email": user.email,
-        }
-    })
+  res.json({
+    accessToken,
+    user: {
+      email: user.email,
+    },
+  });
 };
 
-const logout = async(req, res) => {
-    const {_id} = req.user;
+const logout = async (req, res) => {
+  const { _id } = req.user;
+  console.log("logout:", _id);
 
-    await User.findByIdAndUpdate(_id, {accessToken: ""});
+  await User.findByIdAndUpdate(_id, { accessToken: "" });
 
-    res.status(204).json({message: "logout success"});
-}
+  res.status(204).json({ message: "logout success" });
+};
 
 module.exports = {
-    register,
-    login,
-    logout,
-}
+  register,
+  login,
+  logout,
+};
--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -15,14 +15,18 @@ const authenticate = async (req, res, next) => {
   }
   try {
     const { id } = jwt.verify(token, SECRET_KEY);
+    console.log(id);
     const user = await User.findById(id);
+    console.log(user);
     if (!user || !user.accessToken) {
+      console.log("!user || !user.accessToken");
       next(RequestError(401));
     }
 
     req.user = user;
     next();
   } catch (error) {
+    console.log(error.message);
     next(RequestError(401, error.message));
   }
 };
