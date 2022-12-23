99a4b16780345f2c865617e87a372d976efc470a
Sergey Gulaga <ry6ens@gmail.com>
2022-12-23 17:57:17 +0200

    update transition

--- models/transition.js
+++ models/transition.js
@@ -1,83 +1,94 @@
 const Joi = require("joi");
-const {Schema, model} = require('mongoose');
+const { Schema, model } = require("mongoose");
 
-const {handleSaveErrors} = require("../helpers");
+const { handleSaveErrors } = require("../helpers");
 
-const transitionName = ['income', 'expenses'];
-const transitionCategory = ['', 'Products', 'Alcohol', 'Entertainment', 'Health', 'Transport', 'Housing', 'Technique', 'Communal, communication', 'Sports, hobbies', 'Education', 'Other', 'Salary', 'Add. income']
+const transitionName = ["income", "expenses"];
+const transitionCategory = [
+  "",
+  "Products",
+  "Alcohol",
+  "Entertainment",
+  "Health",
+  "Transport",
+  "Housing",
+  "Technique",
+  "Communal, communication",
+  "Sports, hobbies",
+  "Education",
+  "Other",
+  "Salary",
+  "Add. income",
+];
 
-const transitionSchema = new Schema({
+const transitionSchema = new Schema(
+  {
     transitionName: {
-        type: String,
-        required: [true, 'Set transition name'],
-        enum: transitionName,
+      type: String,
+      required: [true, "Set transition name"],
+      enum: transitionName,
     },
     transitionDate: {
-        type: Date,
-        required: [true, 'Set transition date'],
+      type: String,
+      required: [true, "Set transition date"],
     },
     reportDate: {
-        type: String,
-        default: "",
+      type: String,
+      default: "",
     },
     transitionCategory: {
-        type: String,
-        required: [true, 'Set transition category'],
+      type: String,
+      required: [true, "Set transition category"],
     },
     transitionValue: {
-        type: Number,
-        required: [true, 'Set transition sum'],
+      type: Number,
+      required: [true, "Set transition sum"],
     },
     transitionDescription: {
-        type: String,
-        required: [true, 'Set transition description'],
+      type: String,
+      required: [true, "Set transition description"],
     },
     owner: {
-        type: Schema.Types.ObjectId,
-        ref: "user",
-        required: true,
-    }
-}, {versionKey: false, timestamps: true});
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
 
-transitionSchema.post('save', handleSaveErrors);
+transitionSchema.post("save", handleSaveErrors);
 
 const addTransitionSchema = Joi.object({
-    transitionName: Joi.string()
-            .valid(...transitionName)
-            .required(),
-    transitionDate: Joi.string()
-            .required(),
-    transitionCategory: Joi.string()
-            .required(),
-    transitionValue: Joi.number()
-            .required(),
-    transitionDescription: Joi.string()
-            .required(),
-})
+  transitionName: Joi.string()
+    .valid(...transitionName)
+    .required(),
+  transitionDate: Joi.string().required(),
+  transitionCategory: Joi.string().required(),
+  transitionValue: Joi.number().required(),
+  transitionDescription: Joi.string().required(),
+});
 
 const reqDateSchema = Joi.object({
-    reqDate: Joi.string()
-            .required(),
+  reqDate: Joi.string().required(),
 });
 
 const reqDateAndCategorySchema = Joi.object({
-    reqDate: Joi.string()
-            .required(),
-    transitionCategory: Joi.string()
-            .valid(...transitionCategory)
-            .required(),
-
+  reqDate: Joi.string().required(),
+  transitionCategory: Joi.string()
+    .valid(...transitionCategory)
+    .required(),
 });
 
 const schemas = {
-    addTransitionSchema,
-    reqDateSchema,
-    reqDateAndCategorySchema,
-}
+  addTransitionSchema,
+  reqDateSchema,
+  reqDateAndCategorySchema,
+};
 
-const Transition = model('transition', transitionSchema);
+const Transition = model("transition", transitionSchema);
 
 module.exports = {
-    Transition,
-    schemas,
+  Transition,
+  schemas,
 };
