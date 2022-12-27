16ba3230317e27e1d1552e1cb8505ff424161c0b
Sergey Gulaga <ry6ens@gmail.com>
2022-12-27 16:28:29 +0200

    remova schema

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,21 +1,48 @@
 const express = require("express");
 const { ctrlWrapper } = require("../../helpers");
 const ctrl = require("../../controllers/authController");
-const { validateBody, authenticate, upload, isValidId } = require("../../middlewares");
+const {
+  validateBody,
+  authenticate,
+  upload,
+  isValidId,
+} = require("../../middlewares");
 const { schemas } = require("../../models/user");
 const router = express.Router();
 
 // signup
-router.post("/signup", validateBody(schemas.registerSchema), ctrlWrapper(ctrl.register));
+router.post(
+  "/signup",
+  validateBody(schemas.registerSchema),
+  ctrlWrapper(ctrl.register)
+);
 // login
-router.post("/login", validateBody(schemas.loginSchema), ctrlWrapper(ctrl.login));
+router.post(
+  "/login",
+  validateBody(schemas.loginSchema),
+  ctrlWrapper(ctrl.login)
+);
 // google login
-router.post("/google/signup", validateBody(schemas.googleSchema), ctrlWrapper(ctrl.googleSignup));
+router.post(
+  "/google/signup",
+  validateBody(schemas.googleSchema),
+  ctrlWrapper(ctrl.googleSignup)
+);
 
 router.get("/logout", authenticate, ctrlWrapper(ctrl.logout));
 
-router.patch("/update", authenticate, upload.single("avatar"), validateBody(schemas.updateUserSchema), ctrlWrapper(ctrl.updateUserController));
+router.patch(
+  "/update",
+  authenticate,
+  upload.single("avatar"),
+  ctrlWrapper(ctrl.updateUserController)
+);
 
-router.delete("/:userId", authenticate, isValidId, ctrlWrapper(ctrl.deleteUserController))
+router.delete(
+  "/:userId",
+  authenticate,
+  isValidId,
+  ctrlWrapper(ctrl.deleteUserController)
+);
 
 module.exports = router;
